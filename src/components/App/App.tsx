e6660be4bfad66f96ca104a1e389e5752d6887cd
Mark <mark@remarkablemark.org>
2026-03-28 20:36:34 -0400

    fix: don't center app

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { Converter } from '../Converter';
 
 export function App() {
   return (
-    <main className="mx-auto min-h-screen w-full max-w-6xl px-4 py-6 sm:px-6 lg:px-8">
+    <main className="px-4 py-6 sm:px-6 lg:px-8">
       <Converter />
     </main>
   );
